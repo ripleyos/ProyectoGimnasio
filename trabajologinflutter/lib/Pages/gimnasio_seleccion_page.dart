96f73f313f413f70a532ff86a8ff52ff6b1371a1
ripleyos <sergiogol200@gmail.com>
2024-04-24 14:01:42 +0200

    ya van los marcadores y la funcion ontap funciona con este, falta obteenr la informacion a traves de firebase, cambiar la vista de la pagina, y poner una funcion que guarde un booleano en el movil para que una vez selecionado el gym no te vuelva a salir la seleccion de mapa

---
+++ trabajologinflutter/lib/Pages/gimnasio_seleccion_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class Gym {
+  final String name;
+  final String description;
+
+  Gym({required this.name, required this.description});
+}
+
+class GymDetailsPage extends StatelessWidget {
+  final Gym gym;
+
+  GymDetailsPage({required this.gym});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(gym.name),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              gym.name,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Text(
+              gym.description,
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción para el botón "Sí"
+                  },
+                  child: Text('Sí'),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción para el botón "No"
+                  },
+                  child: Text('No'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- trabajologinflutter/lib/Pages/mapa_page.dart
+++ trabajologinflutter/lib/Pages/mapa_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:trabajologinflutter/Pages/gimnasio_seleccion_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MapPage extends StatelessWidget {
@@ -17,6 +18,26 @@ Widget build(BuildContext context) {
       TileLayer(
         urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
         userAgentPackageName: 'com.example.app',
+      
+      ),
+      GestureDetector(
+        onTap: (){
+          Gym gym = Gym(name: 'Nombre del gimnasio 1', description: 'Descripción del gimnasio 1');
+          Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => GymDetailsPage(gym: gym)),
+        );
+        },
+          child: const MarkerLayer(
+          markers: [
+            Marker(
+              point: LatLng(38.028041, -0.746493),
+              width: 400,
+              height: 400,
+              child: FlutterLogo(size: 50),
+              ),
+            ],
+          ),
       ),
       RichAttributionWidget(
         attributions: [
